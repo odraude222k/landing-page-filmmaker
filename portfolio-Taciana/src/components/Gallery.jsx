58548c293ec2bd6f1a9e7dc30728ea41c69c90aa
odraude222k <pereirabentoeduardo@gmail.com>
2026-04-27 17:06:56 -0300

    feat: adiciona arquivo Gallery e suas funcionalidades

---
+++ portfolio-Taciana/src/components/Gallery.jsx
@@ -0,0 +1,8 @@
+export default function Gallery() {
+  return (
+    <section id="gallery">
+      <h2>Portfólio</h2>
+      <p>[ Aqui vai entrar o grid de fotos e vídeos ]</p>
+    </section>
+  )
+}
