9f4637cd9bd3ce805a0a888c43eaa4f8d0708aff
odraude222k <pereirabentoeduardo@gmail.com>
2026-04-27 17:06:11 -0300

    feat: adiciona arquivo Contact e suas funcionalidades

---
+++ portfolio-Taciana/src/components/Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <section id="contact">
+      <h2>Vamos trabalhar juntos?</h2>
+      <p>Email: contato@emaildela.com</p>
+    </section>
+  )
+}
