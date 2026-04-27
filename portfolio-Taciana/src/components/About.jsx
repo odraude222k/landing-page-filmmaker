6a43854e83b9a5586a817c0b9b630e76c2733f64
odraude222k <pereirabentoeduardo@gmail.com>
2026-04-27 17:05:26 -0300

    feat: adiciona arquivo about e suas funcionalidades

---
+++ portfolio-Taciana/src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <section id="about">
+      <h2>Sobre Mim</h2>
+      <p>Transformando momentos em arte através das lentes.</p>
+    </section>
+  )
+}
