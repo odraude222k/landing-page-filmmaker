8834ba8934b419eda81969fd4dd1c0723c9f93f7
odraude222k <pereirabentoeduardo@gmail.com>
2026-04-27 17:07:33 -0300

    feat: adiciona arquivo Hero e suas funcionalidades

---
+++ portfolio-Taciana/src/components/Hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <section id="hero">
+      <h1>Nome da Sua Namorada</h1>
+      <h2>Filmmaker & Fotógrafa</h2>
+      <button>Conheça meu trabalho</button>
+    </section>
+  )
+}
