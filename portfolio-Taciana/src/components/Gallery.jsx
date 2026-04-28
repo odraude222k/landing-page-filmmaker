7e8a1048e881d6a776ccae16954ecae00c3aa76a
odraude222k <pereirabentoeduardo@gmail.com>
2026-04-28 17:21:28 -0300

    style: tirando os comentários

--- portfolio-Taciana/src/components/Gallery.jsx
+++ portfolio-Taciana/src/components/Gallery.jsx
@@ -4,7 +4,6 @@ export default function Gallery() {
       <div className="max-w-6xl mx-auto">
         <h2 className="text-3xl font-bold mb-12 text-center">Portfólio</h2>
         
-        {/* Aqui é o esqueleto do Grid de fotos */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <div className="bg-zinc-800 aspect-square rounded-lg flex items-center justify-center text-zinc-500">Foto/Vídeo 1</div>
           <div className="bg-zinc-800 aspect-square rounded-lg flex items-center justify-center text-zinc-500">Foto/Vídeo 2</div>
