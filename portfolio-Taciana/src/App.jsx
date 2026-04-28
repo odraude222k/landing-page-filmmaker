82872906b42045faaf54430be519b2b815b5a406
odraude222k <pereirabentoeduardo@gmail.com>
2026-04-28 16:42:33 -0300

    fix: altera o codigo para o uso do tailwind css

--- portfolio-Taciana/src/App.jsx
+++ portfolio-Taciana/src/App.jsx
@@ -5,7 +5,7 @@ import Contact from './components/Contact'
 
 function App() {
   return (
-    <div>
+    <div className="bg-zinc-900 text-zinc-100 min-h-screen font-sans">
       <Hero />
       <About />
       <Gallery />
