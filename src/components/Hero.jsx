0429b71001bf9faeaff8c2612bb8d661098b4f35
odraude222k <pereirabentoeduardo@gmail.com>
2026-04-30 15:12:52 -0300

    feat: alterando a cor para a palheta escolhida

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,10 @@
 export default function Hero() {
   return (
     <section id="hero" className="w-full h-screen flex flex-col justify-center items-center text-center">
-     
-      <h1 className="text-6xl md:text-[120px] font-bold mb-4 tracking-tighter leading-none uppercase">
-        Capturando <br /> <span className="text-zinc-500">Momentos</span>
+      <h1 className="text-6xl md:text-[120px] font-bold mb-4 tracking-tighter leading-none uppercase text-brand-offwhite">
+        Capturando <br /> <span className="text-brand-offwhite/50">Momentos</span>
       </h1>
-      <p className="text-zinc-400 text-lg md:text-xl font-light tracking-widest uppercase mt-8">
+      <p className="text-brand-offwhite/60 text-lg md:text-xl font-light tracking-widest uppercase mt-8">
         Audiovisual & Fotografia Profissional
       </p>
     </section>
