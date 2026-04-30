0795d6f89d185ccd53c3441c21b7981df4d299ec
odraude222k <pereirabentoeduardo@gmail.com>
2026-04-30 11:18:56 -0300

    test: faz o teste da navbar

--- portfolio-Taciana/tests/Navbar.test.jsx
+++ portfolio-Taciana/tests/Navbar.test.jsx
@@ -1,16 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-
-// 1. O caminho de importação foi ajustado para refletir a nova organização de pastas
+import '@testing-library/jest-dom/vitest'; // Traz o "poder" do toBeInTheDocument
 import Navbar from '../src/components/Navbar'; 
 
 describe('Integração: Navbar', () => {
   it('deve renderizar a marca/nome corretamente na tela', () => {
+    // 1. O Vitest "monta" a Navbar no nosso navegador simulado (jsdom)
     render(<Navbar />);
     
-    // Lembre-se de manter a palavra exata que está no seu código real
+    // 2. Procura pelo texto "TACIANA" (o /i ignora se é maiúscula ou minúscula)
     const marca = screen.getByText(/taciana/i); 
     
+    // 3. Verifica se existe
     expect(marca).toBeInTheDocument();
   });
 });
