d6bdaeca62bccdc79ea4f51efffe193e7912fc74
odraude222k <pereirabentoeduardo@gmail.com>
2026-04-30 14:38:01 -0300

    test: adiciona teste dos botoes da Navbar

--- tests/Navbar.test.jsx
+++ tests/Navbar.test.jsx
@@ -1,17 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
-import '@testing-library/jest-dom/vitest'; // Traz o "poder" do toBeInTheDocument
+import { render, screen,cleanup } from '@testing-library/react';
+import { describe, it, expect, afterEach } from 'vitest';
+import '@testing-library/jest-dom/vitest';
 import Navbar from '../src/components/Navbar'; 
 
 describe('Integração: Navbar', () => {
+  
+  afterEach(() => {
+    cleanup();
+  });
+  
   it('deve renderizar a marca/nome corretamente na tela', () => {
-    // 1. O Vitest "monta" a Navbar no nosso navegador simulado (jsdom)
     render(<Navbar />);
-    
-    // 2. Procura pelo texto "TACIANA" (o /i ignora se é maiúscula ou minúscula)
     const marca = screen.getByText(/taciana/i); 
-    
-    // 3. Verifica se existe
     expect(marca).toBeInTheDocument();
   });
+
+  it('deve conter os links de navegação sobre com os destinos corretos', () => {
+    render(<Navbar />);
+    
+    const linkSobre = screen.getByText('Sobre');
+    const linkPortfolio = screen.getByText('Portfólio');
+    const linkContato = screen.getByText('Contato');
+
+    expect(linkSobre).toBeInTheDocument();
+    expect(linkSobre).toHaveAttribute('href', '#about');
+
+    expect(linkPortfolio).toBeInTheDocument();
+    expect(linkPortfolio).toHaveAttribute('href', '#gallery');
+
+    expect(linkContato).toBeInTheDocument();
+    expect(linkContato).toHaveAttribute('href', '#contact');
+  });
+  
 });
