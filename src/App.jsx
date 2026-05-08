e23067eed0c347571123072839c9f87b83812197
Eduardo <pereirabentoeduardo@gmail.com>
2026-05-08 18:50:06 -0300

    style: comenta o import que não esta utilizando no momento

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Services from './components/Services';
 import About from './components/About';
 // NOVOS IMPORTS
 import Process from './components/Process';
-import Testimonials from './components/Testimonials';
+/*import Testimonials from './components/Testimonials';*/
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
